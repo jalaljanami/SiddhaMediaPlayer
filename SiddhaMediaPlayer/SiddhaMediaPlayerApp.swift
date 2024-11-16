b7504d107a33b8c0e59a7a8b6492aa0aff3ce8a4
jalal <jalal.janami@yahoo.com>
2024-11-16 22:43:40 +0330

    Initial Commit

---
+++ SiddhaMediaPlayer/SiddhaMediaPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiddhaMediaPlayerApp.swift
+//  SiddhaMediaPlayer
+//
+//  Created by jalal on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SiddhaMediaPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
